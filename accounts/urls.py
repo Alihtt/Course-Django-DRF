7f7ae4af40d0c7172dec629c9f893ebcb65bcb3d
Aliht <aliht.workspace@gmail.com>
2024-01-23 16:28:58 +0330

    authentication token

--- accounts/urls.py
+++ accounts/urls.py
@@ -1,8 +1,10 @@
 from django.urls import path
+from rest_framework.authtoken import views as auth_token
 from . import views
 
 app_name = 'accounts'
 
 urlpatterns = [
-    path('register/', views.UserRegister.as_view(), name='user_register')
+    path('register/', views.UserRegister.as_view(), name='user_register'),
+    path('api-token-auth/', auth_token.obtain_auth_token),
 ]
