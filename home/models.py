2924868f97117e5a38a8b714e0ed1914c8c9d42b
Aliht <aliht.workspace@gmail.com>
2024-01-26 20:00:44 +0330

    add question view

--- home/models.py
+++ home/models.py
@@ -1,4 +1,5 @@
 from django.db import models
+from django.contrib.auth.models import User
 
 
 class Person(models.Model):
@@ -8,3 +9,29 @@ class Person(models.Model):
 
     def __str__(self):
         return self.name
+
+
+class Question(models.Model):
+    user = models.ForeignKey(
+        User, on_delete=models.CASCADE, related_name='questions')
+    title = models.CharField(max_length=200)
+    slug = models.SlugField(max_length=200)
+    created = models.DateTimeField(auto_now_add=True)
+    body = models.TextField()
+
+    def __str__(self) -> str:
+        return f'{self.user.username} - {self.title[:20]}'
+
+
+class Answer(models.Model):
+    user = models.ForeignKey(
+        User, on_delete=models.CASCADE, related_name='answers')
+    question = models.ForeignKey(
+        Question, on_delete=models.CASCADE, related_name='answers')
+    title = models.CharField(max_length=200)
+    slug = models.SlugField(max_length=200)
+    created = models.DateTimeField(auto_now_add=True)
+    body = models.TextField()
+
+    def __str__(self) -> str:
+        return f'{self.user.username} - {self.title[:20]}'
--- home/views.py
+++ home/views.py
@@ -1,8 +1,9 @@
 from rest_framework.views import APIView
 from rest_framework.response import Response
-from .models import Person
-from .serializers import PersonSerializer
+from .models import Person, Question
+from .serializers import PersonSerializer, QuestionSerializer
 from rest_framework.permissions import IsAuthenticated
+from rest_framework import status
 
 
 class Home(APIView):
@@ -12,3 +13,31 @@ class Home(APIView):
         persons = Person.objects.all()
         ser_data = PersonSerializer(instance=persons, many=True)
         return Response(data=ser_data.data)
+
+
+class QuestionView(APIView):
+    def get(self, request):
+        questions = Question.objects.all()
+        ser_data = QuestionSerializer(instance=questions, many=True)
+        return Response(data=ser_data.data, status=status.HTTP_200_OK)
+
+    def post(self, request):
+        ser_data = QuestionSerializer(data=request.data)
+        if ser_data.is_valid():
+            ser_data.save()
+            return Response(data=ser_data.data, status=status.HTTP_201_CREATED)
+        return Response(data=ser_data.errors, status=status.HTTP_400_BAD_REQUEST)
+
+    def put(self, request, pk):
+        question = Question.objects.get(pk=pk)
+        ser_data = QuestionSerializer(
+            instance=question, data=request.data, partial=True)
+        if ser_data.is_valid():
+            ser_data.save()
+            return Response(data=ser_data.data, status=status.HTTP_200_OK)
+        return Response(data=ser_data.errors, status=status.HTTP_400_BAD_REQUEST)
+
+    def delete(self, request, pk):
+        question = Question.objects.get(pk=pk)
+        question.delete()
+        return Response(data={'message': 'question deleted'}, status=status.HTTP_200_OK)
