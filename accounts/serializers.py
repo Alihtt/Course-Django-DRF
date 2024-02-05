40e2ac9ec5b5e009bdc39f639019aed63c85c40b
Aliht <aliht.workspace@gmail.com>
2024-02-05 17:04:46 +0330

    viewsets and routers

--- accounts/serializers.py
+++ accounts/serializers.py
@@ -3,8 +3,8 @@ from django.contrib.auth.models import User
 
 
 def clean_email(data):
-    if 'admin' in data.lower():
-        raise serializers.ValidationError('email cant be `admin`')
+    if "admin" in data.lower():
+        raise serializers.ValidationError("email cant be `admin`")
     else:
         return data
 
@@ -14,24 +14,30 @@ class UserRegisterSerializer(serializers.ModelSerializer):
 
     class Meta:
         model = User
-        fields = ('username', 'email', 'password', 'password_confirm')
+        fields = ("username", "email", "password", "password_confirm")
         extra_kwargs = {
-            'email': {'validators': (clean_email,)},
-            'password': {'write_only': True}
+            "email": {"validators": (clean_email,)},
+            "password": {"write_only": True},
         }
 
     def create(self, validated_data):
-        del validated_data['password_confirm']
+        del validated_data["password_confirm"]
         return User.objects.create_user(**validated_data)
 
     def validate_username(self, data):
-        if data.lower() == 'admin':
-            raise serializers.ValidationError('Username cant be `admin`')
+        if data.lower() == "admin":
+            raise serializers.ValidationError("Username cant be `admin`")
         else:
             return data
 
     def validate(self, data):
-        if data['password'] != data['password_confirm']:
-            raise serializers.ValidationError('Passwords must match')
+        if data["password"] != data["password_confirm"]:
+            raise serializers.ValidationError("Passwords must match")
         else:
             return data
+
+
+class UserSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = User
+        fields = "__all__"
