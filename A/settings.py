e09c11811998df5d5570552cdb3e8e5adf27c80d
Aliht <aliht.workspace@gmail.com>
2024-02-06 17:15:21 +0330

    render/parser

--- A/settings.py
+++ A/settings.py
@@ -137,6 +137,9 @@ REST_FRAMEWORK = {
     "DEFAULT_THROTTLE_RATES": {
         "questions": "5/hour",
     },
+    'DEFAULT_RENDERER_CLASSES': [
+        'rest_framework.renderers.JSONRenderer',
+    ]
 }
 
 SPECTACULAR_SETTINGS = {
